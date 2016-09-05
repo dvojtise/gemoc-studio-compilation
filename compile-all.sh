e13182cedb3d926e66683554f3d1f3c36b3ad938
Erwan Bousse <ebousse@ebousse-isis-tuwien-ac-at.lan>
2016-09-05 12:21:14 +0200

    Early lines of script for compiling gemoc projects

---
+++ compile-all.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+set -e
+
+LOCALREPO="$PWD/localm2"
+MVN_CMD_NOPROFILES="mvn install -Dmaven.repo.local=$LOCALREPO"
+MVN_CMD="$MVN_CMD_NOPROFILES -P ignore_CI_repositories"
+PROGRESSMESSAGE="############## now in $PWD ##################"
+
+# Make sure all the submodules are up-to-date
+git submodule update --remote
+
+# we follow this order: https://ci.inria.fr/gemoc/job/gemoc-studio_gemoc_studio/depgraph-view/
+
+cd gemoc-studio
+cd commons 
+echo $PROGRESSMESSAGE
+$MVN_CMD
+
+cd ../gemoc_commons 
+echo $PROGRESSMESSAGE
+$MVN_CMD
+
+cd ../../ModelDebugging 
+echo $PROGRESSMESSAGE
+$MVN_CMD_NOPROFILES -P ignore_CI_repositories,trace_and_framework
+
+
+
+
+
