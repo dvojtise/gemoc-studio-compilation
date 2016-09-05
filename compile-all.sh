2d86f341d9fb0a99090bc66fe580d7745e4e3967
Erwan Bousse <ebousse@ebousse-isis-tuwien-ac-at.lan>
2016-09-05 14:58:49 +0200

    Replace manual script by maven submodules

--- compile-all.sh
+++ compile-all.sh
@@ -2,29 +2,10 @@
 set -e
 
 LOCALREPO="$PWD/localm2"
-MVN_CMD_NOPROFILES="mvn install -Dmaven.repo.local=$LOCALREPO"
-MVN_CMD="$MVN_CMD_NOPROFILES -P ignore_CI_repositories"
-PROGRESSMESSAGE="############## now in $PWD ##################"
 
 # Make sure all the submodules are up-to-date
 # git submodule update --remote
 
-# we follow this order: https://ci.inria.fr/gemoc/job/gemoc-studio_gemoc_studio/depgraph-view/
-
-cd gemoc-studio
-cd commons 
-echo $PROGRESSMESSAGE
-$MVN_CMD
-
-cd ../gemoc_commons 
-echo $PROGRESSMESSAGE
-$MVN_CMD
-
-cd ../../ModelDebugging 
-echo $PROGRESSMESSAGE
-$MVN_CMD_NOPROFILES -P ignore_CI_repositories,trace_and_framework
-
-
-
+mvn package -Dmaven.repo.local=$LOCALREPO -P 'ignore_CI_repositories,!use_CI_repositories'
 
 
